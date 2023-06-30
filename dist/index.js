2ed50e771ed2b6f430a35a9f93babea37225b798
Himal Gautam <himalpgautam@gmail.com>
2023-06-30 18:13:44 +0530

    tuples

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,13 @@
 "use strict";
-let age = 20;
-if (age < 50)
-    age += 10;
-console.log(age);
+let sales = 123456789;
+let course = "TypeScript";
+let is_published = true;
+let level;
+level = 1;
+level = "a";
+function render(document) {
+    console.log(document);
+}
+let numbers = [1, 2, 3];
+let user = [1, 'Himal'];
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,22 @@
 // BUILT-IN DATA TYPES
 // =================================================================
 
-// let sales = 123_456_789; //auto detect type number
-// let course: string = "TypeScript";
-// let is_published: boolean = true;
+let sales = 123_456_789; //auto detect type number
+let course: string = "TypeScript";
+let is_published: boolean = true;
 
 // =================================================================
 // "ANY" DATA TYPE
 // =================================================================
 
-// let level; //type any
-// level = 1;
-// level = "a"; //not give errors
+let level; //type any
+level = 1;
+level = "a"; //not give errors
 
 // function render(document) { // implicitly have type any
-// function render(document: any) { // explicitly have type any
-//   console.log(document);
-// }
+function render(document: any) { // explicitly have type any
+  console.log(document);
+}
 
 // =================================================================
 // ARRAYS
@@ -25,4 +25,10 @@
 
 // let numbers = [1, 2, '3']; // implicitly have type any[]
 // let numbers : any[] = [1, 2, '3']; // explicitly have type any
-let numbers : number[] = [1, 2, 3]; // implicitly have type any 
+let numbers : number[] = [1, 2, 3]; // implicitly have type any
+
+// =================================================================
+// TUPLES
+// =================================================================
+
+let user: [number, string] = [1, 'Himal'];
