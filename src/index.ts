fc778daec7f76b68506749bc4564f7b53f6b533d
Himal Gautam <himalpgautam@gmail.com>
2023-06-30 18:39:04 +0530

    functions

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,8 @@ level = 1;
 level = "a"; //not give errors
 
 // function render(document) { // implicitly have type any
-function render(document: any) { // explicitly have type any
+function render(document: any) {
+  // explicitly have type any
   console.log(document);
 }
 
@@ -25,24 +26,39 @@ function render(document: any) { // explicitly have type any
 
 // let numbers = [1, 2, '3']; // implicitly have type any[]
 // let numbers : any[] = [1, 2, '3']; // explicitly have type any
-let numbers : number[] = [1, 2, 3]; // implicitly have type any
+let numbers: number[] = [1, 2, 3]; // implicitly have type any
 
 // =================================================================
 // TUPLES
 // =================================================================
 
-let user: [number, string] = [1, 'Himal'];
+let user: [number, string] = [1, "Himal"];
 
 // =================================================================
 // ENUMS
 // =================================================================
 
 // enum Size {Small, Medium, Large} //{Small = 0, Medium = 1, Large = 2}
-enum Size {Small = 1 , Medium, Large} //{Small = 1, Medium = 2, Large = 3}
+// enum Size {Small = 1 , Medium, Large} //{Small = 1, Medium = 2, Large = 3}
+const enum Size {
+  Small = 1,
+  Medium,
+  Large,
+} //more optimised
 // enum Size {Small = 's' , Medium = 'm', Large = 'l'} //declare for all sizes
 
 let mySize: Size = Size.Medium;
 console.log(mySize); //2
 
+// =================================================================
+// FUNCTIONS
+// =================================================================
 
+// function calculateTax(income: number) {} // return void
+// function calculateTax(income: number){return 0;} //implicitly return type number
+function calculateTax(income: number, taxYear = 2022): number {
+  if (taxYear < 50_000) return income * 1.2;
+  return income * 1.3;
+} //explicitly return type number
 
+calculateTax(10_000, 2022)
