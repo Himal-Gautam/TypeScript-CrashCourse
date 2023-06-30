094e5e21daba1a2c46b29b58adcc2cf7d1d2bb43
Himal Gautam <himalpgautam@gmail.com>
2023-06-30 15:58:34 +0530

    any data type

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
-let sales = 123_456_789; //auto detect type number
-let course: string = 'TypeScript';
-let is_published: boolean = true;
-let level  //type any
+// =================================================================
+// BUILT-IN DATA TYPES
+// =================================================================
+
+// let sales = 123_456_789; //auto detect type number
+// let course: string = "TypeScript";
+// let is_published: boolean = true;
+
+// =================================================================
+// "ANY" DATA TYPE
+// =================================================================
+
+// let level; //type any
+// level = 1;
+// level = "a"; //not give errors
+
+// function render(document) { // implicitly have type any
+// function render(document: any) { // explicitly have type any
+//   console.log(document);
+// }
 
