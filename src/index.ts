e4a6a33ffa23132d62f990e84d5ff44034dbe99b
Himal Gautam <himalpgautam@gmail.com>
2023-06-30 15:52:15 +0530

    built-in types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-let age: number = 20;
-if (age < 50)
-    age += 10
-    console.log(age)
+let sales = 123_456_789; //auto detect type number
+let course: string = 'TypeScript';
+let is_published: boolean = true;
+let level  //type any
+
